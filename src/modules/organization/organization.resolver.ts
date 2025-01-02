08a4e8d55adc9dce6e65c6c141051f743c0d491d
Eason Sun <ballentin133@outlook.com>
2025-01-02 22:20:28 +0800

    feat: 优化组织的代码

--- src/modules/organization/organization.resolver.ts
+++ src/modules/organization/organization.resolver.ts
@@ -3,33 +3,57 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 // import { UseGuards } from '@nestjs/common';
 // import { GqlAuthGuard } from 'src/modules/common/guards/auth.guard';
 import { OrganizationService } from './organization.service';
-import { MutationOrganizationInput } from 'src/graphql.schema';
+import {
+  MutationOrganizationInput,
+  OrganizationResponse,
+  Response,
+} from 'src/graphql.schema';
+import Res from 'src/utils/response';
 
 @Resolver()
 // @UseGuards(GqlAuthGuard)
 export class OrganizationResolver {
-  constructor(private readonly OrganizationService: OrganizationService) {}
+  constructor(private readonly organizationService: OrganizationService) {}
 
   @Mutation('createOrganization')
-  async users(@Args('input') input: MutationOrganizationInput) {
-    return this.OrganizationService.createOrganization(input);
+  async users(
+    @Args('input') input: MutationOrganizationInput,
+  ): Promise<OrganizationResponse> {
+    const data = await this.organizationService.createOrganization(input);
+    if (!data) return new Res(false, 'create organization failed');
+    return {
+      organization: data,
+      ...new Res(true),
+    };
   }
 
   @Mutation('updateOrganization')
   async updateOrganization(
     @Args('id') id: string,
     @Args('input') input: MutationOrganizationInput,
-  ) {
-    return this.OrganizationService.updateOrganization(id, input);
+  ): Promise<OrganizationResponse> {
+    const data = await this.organizationService.updateOrganization(id, input);
+    if (!data) return new Res(false, 'not fond organization');
+    return {
+      organization: data,
+      ...new Res(true),
+    };
   }
 
   @Mutation('removeOrganization')
-  async removeOrganization(@Args('id') id: string) {
-    return this.OrganizationService.removeOrganization(id);
+  async removeOrganization(@Args('id') id: string): Promise<Response> {
+    const data = await this.organizationService.removeOrganization(id);
+    if (!data) return new Res(false, 'not fond organization').response;
+    return new Res(true).response;
   }
 
   @Query('getOrganization')
-  async getOrganization(@Args('id') id: string) {
-    return this.OrganizationService.getOrganization(id);
+  async getOrganization(@Args('id') id: string): Promise<OrganizationResponse> {
+    const data = await this.organizationService.getOrganization(id);
+    if (!data) return new Res(false, 'not fond organization');
+    return {
+      organization: data,
+      ...new Res(true),
+    };
   }
 }
--- src/modules/organization/organization.service.ts
+++ src/modules/organization/organization.service.ts
@@ -1,93 +1,62 @@
 import { Injectable } from '@nestjs/common';
-import {
-  MutationOrganizationInput,
-  Organization,
-  OrganizationResponse,
-  Response,
-} from 'src/graphql.schema';
+import { MutationOrganizationInput } from 'src/graphql.schema';
 import { PrismaService } from 'src/modules/prisma/prisma.service';
-import Res from 'src/utils/response';
 
 @Injectable()
 export class OrganizationService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async createOrganization(
-    input: MutationOrganizationInput,
-  ): Promise<OrganizationResponse> {
-    try {
-      const data = this.prisma.organization.create({
-        data: input,
-      });
-      if (!data) {
-        return new Res(false, 'Organization not created');
-      }
-      return {
-        organization: data as unknown as Organization,
-        ...new Res(true),
-      };
-    } catch (e) {
-      return new Res(false, e.message);
+  async createOrganization(input: MutationOrganizationInput) {
+    const data = await this.prisma.organization.create({
+      data: input,
+    });
+    if (!data) {
+      return null;
     }
+    return data;
   }
 
-  async updateOrganization(
-    id: string,
-    input: MutationOrganizationInput,
-  ): Promise<OrganizationResponse> {
-    try {
-      const data = this.prisma.organization.update({
-        where: {
-          id: id,
-        },
-        data: input,
-      });
-      if (!data) {
-        return new Res(false, 'Organization not found');
-      }
-      return {
-        organization: data as unknown as Organization,
-        ...new Res(true),
-      };
-    } catch (e) {
-      return new Res(false, e.message);
+  async updateOrganization(id: string, input: MutationOrganizationInput) {
+    const data = await this.prisma.organization.update({
+      where: {
+        id: id,
+      },
+      data: input,
+    });
+    if (!data) {
+      return null;
     }
+    return data;
   }
 
-  async removeOrganization(id: string): Promise<Response> {
-    try {
-      const data = this.prisma.organization.delete({
-        where: {
-          id: id,
-        },
-      });
-      if (!data) {
-        return new Res(false, 'Organization not found').response;
-      }
-      return new Res(true).response;
-    } catch (e) {
-      return new Res(false, e.message).response;
+  async removeOrganization(id: string) {
+    const data = await this.prisma.organization.update({
+      where: {
+        id: id,
+        deletedAt: null,
+      },
+      data: {
+        deletedAt: new Date(),
+      },
+    });
+    if (!data) {
+      return null;
     }
+    return true;
   }
 
-  async getOrganization(id: string): Promise<OrganizationResponse> {
-    try {
-      const data = this.prisma.organization.findUnique({
-        where: {
-          id: id,
-        },
-      });
+  async getOrganization(id: string) {
+    const data = await this.prisma.organization.findUnique({
+      where: {
+        id: id,
+        deletedAt: null,
+      },
+    });
 
-      if (!data) {
-        return new Res(false, 'Organization not found');
-      }
-
-      return {
-        organization: data as unknown as Organization,
-        ...new Res(true),
-      };
-    } catch (e) {
-      return new Res(false, e.message);
+    if (!data) {
+      return null;
     }
+
+    return data;
   }
 }
